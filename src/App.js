8be673cfa4257215279be36db06924fb24e8bf56
Nathan Brickett <nathanbrickett@gmail.com>
2025-06-12 19:57:39 -0700

    feat: add dark mode toggle

--- src/App.js
+++ src/App.js
@@ -7,33 +7,59 @@ import { PROJECTS } from "./components/GameBoyColor/gbcConstants";
 
 function App() {
   const [useSimpleMenu, setUseSimpleMenu] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
     <>
-      <div>
-        <button
-          onClick={() => setUseSimpleMenu(!useSimpleMenu)}
-          style={{
-            position: "fixed",
-            top: "90%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            zIndex: 1001,
-            padding: "10px 16px",
-            fontSize: "14px",
-            backgroundColor: "#E7115F",
-            color: "white",
-            borderRadius: "5px",
-            cursor: "pointer",
-            marginTop: "20px",
-            border: "1px solid #E7D509",
-          }}
-        >
-          {useSimpleMenu
-            ? "Switch to GameBoy View"
-            : "Switch to Full-Screen Mode"}
-        </button>
-        <Skyline />
+      <div >
+        <div style={{ display: 'flex', flexDirection: 'row'}}>
+          <button
+            onClick={() => setUseSimpleMenu(!useSimpleMenu)}
+            style={{
+              position: "fixed",
+              top: "90%",
+              left: "40%",
+              transform: "translate(-50%, -50%)",
+              zIndex: 1001,
+              padding: "10px 16px",
+              fontSize: "14px",
+              backgroundColor: "#E7115F",
+              color: "white",
+              borderRadius: "5px",
+              cursor: "pointer",
+              marginTop: "20px",
+              border: "1px solid #E7D509",
+            }}
+          >
+            {useSimpleMenu
+              ? "Switch to GameBoy View"
+              : "Switch to Full-Screen Mode"}
+          </button>
+          <button
+            onClick={() => setIsDarkMode(!isDarkMode)}
+            style={{
+              position: "fixed",
+              top: "90%",
+              left: "60%",
+              transform: "translate(-50%, -50%)",
+              zIndex: 1001,
+              padding: "10px 16px",
+              fontSize: "14px",
+              backgroundColor: "#E7115F",
+              color: "white",
+              borderRadius: "5px",
+              cursor: "pointer",
+              marginTop: "20px",
+              border: "1px solid #E7D509",
+            }}
+          >
+            {isDarkMode
+              ? "Switch to Dark Mode"
+              : "Switch to Light Mode"}
+          </button>
+        </div>
+
+        <Skyline darkMode={isDarkMode} />
         {useSimpleMenu ? <SimpleMenu projects={PROJECTS} /> : <GameBoyColor />}
       </div>
     </>
